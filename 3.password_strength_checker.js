ba112bc9233a3aeae5af7af8fc31f663592e3e6a
Johirul Islam Himel <himel1199@gmail.com>
2026-02-05 17:43:15 +0600

    Password Strength Checker

---
+++ 3.password_strength_checker.js
@@ -0,0 +1,66 @@
+/**
+ * Problem 3: Password Strength Checker
+Function name: checkPassword(password)
+Rules:
+Length must be at least 8
+
+
+Must contain at least 1 number
+
+
+Must contain at least 1 uppercase letter
+
+
+Must not contain spaces
+
+
+Test case 1
+Input:  "helloWorld"
+Output:
+{ valid: false, reasons: ["missing number"] }
+
+Test case 2
+Input:  "Hello123"
+Output: { valid: true, reasons: [] }
+
+ */
+
+function checkPassword(password) {
+  let reasons = [];
+  let length = password.length;
+  let hasNumber = false;
+  let hasUpperCase = false;
+  let hasSpace = password.includes(" ");
+  for (let i = 0; i < length; i++) {
+    let character = password[i];
+    // console.log(character);
+    if (character >= "0" && character <= "9") {
+      hasNumber = true;
+    }
+    if (character >= "A" && character <= "Z") {
+      hasUpperCase = true;
+    }
+  }
+  //console.log(password, length, hasUpperCase, hasNumber);
+  if (hasNumber === false) {
+    reasons.push("Missing number");
+  }
+  if (hasUpperCase === false) {
+    reasons.push("Missing uppercase letter");
+  }
+  if (hasSpace === true) {
+    reasons.push("Space found!!!");
+  }
+  //console.log(hasSpace);
+
+  let isValid = reasons.length === 0;
+
+  return {
+    valid: isValid,
+    reasons,
+  };
+}
+let output1 = checkPassword("helloWorld");
+console.log(output1);
+let output2 = checkPassword("Hello123");
+console.log(output2);
